33049c4e86c02e9211f36e34de0d9f036acf44f5
lferreira <lferreira_ext@tozzinifreire.adv.br>
2018-05-29 16:43:24 -0300

    add unit test proect

---
+++ GameStore.Tests/Queries/GameSelectsTests.cs
@@ -0,0 +1,27 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Infra.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace GameStore.Tests
+{
+    public class GameSelectsTests
+    {
+        private GameRepository _repository;
+
+        public GameSelectsTests(GameRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [Fact]
+        public void QueryAllGames()
+        {
+            var list = _repository.GetAllAsync().GetAwaiter().GetResult();
+            Assert.True(list.Count() > 0);
+        }
+    }
+}
+
