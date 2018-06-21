4963b5b6dfece9624896945325c5b84dd70d0cc5
lferreira_ext <lferreira_ext@BLGWLFERREIRA>
2018-06-21 18:20:37 -0300

    add font styles and hero section

--- src/1 - UI/GameStore.UI.Site/GameStore/src/app/app.module.ts
+++ src/1 - UI/GameStore.UI.Site/GameStore/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+import { ROUTES } from "./app.routes";
+import { RouterModule } from '@angular/router';
+import { HeroComponent } from './home/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
