12aae9f5a83c42048db34811b9690351e5836a54
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2018-06-25 22:40:36 -0300

    add parameters to slide

--- src/1 - UI/GameStore.UI.Site/GameStore/src/app/home/hero/hero.component.ts
+++ src/1 - UI/GameStore.UI.Site/GameStore/src/app/home/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroSlide } from './hero-slide.model';
 
 import {
   animate,
@@ -15,30 +16,38 @@ import {
   animations: [
     trigger('carousel', [
       state('slide1', style({
-        'background': 'url(../../../assets/images/hero/wall1.jpg)'
-      })),
+        'background': 'url("{{imagePath}}")'
+      }), { params: { imagePath : '' } }),
       state('slide2', style({
-        'background': 'url(../../../assets/images/hero/wall2.png)'
-      })),
+        'background': 'url("{{imagePath}}")'
+      }), { params: { imagePath : '' } }),
       state('slide3', style({
-        'background': 'url(../../../assets/images/hero/wall3.jpg)'
-      })),
+        'background': 'url("{{imagePath}}")'
+      }), { params: { imagePath : '' } }),
       state('slide4', style({
-        'background': 'url(../../../assets/images/hero/wall4.jpg)'
-      })),
+        'background': 'url("{{imagePath}}")'
+      }), { params: { imagePath : '' } }),
       transition('slide1 => slide2', animate("500ms ease-in")),
       transition('slide2 => slide3', animate("500ms ease-in")),
       transition('slide3 => slide4', animate("500ms ease-in")),
       transition('slide4 => slide1', animate("500ms ease-in"))
     ])
   ]
-  
+
 })
 export class HeroComponent implements OnInit {
+  
+  slideCounter: number = 0;
+  states = ['slide1', 'slide2', 'slide3', 'slide4']
+  slides: HeroSlide[] = new Array<HeroSlide>(4)
+   
+  constructor() {
 
-  slide = 'slide1';
-  slides = ['slide1','slide2','slide3','slide4']
-  constructor() { 
+    //todo: create web service to feed the slides
+    this.slides[0] = { gameName: 'Portal', gameDescription: '', imagePath: '../../../assets/images/hero/wall1.jpg' }
+    this.slides[1] = { gameName: 'Zelda: Breath of the Wild', gameDescription: '', imagePath: '../../../assets/images/hero/wall2.png' },
+    this.slides[2] = { gameName: 'Rise of the Tomb Raider', gameDescription: '', imagePath: '../../../assets/images/hero/wall3.jpg' },
+    this.slides[3] = { gameName: 'Fallout 4', gameDescription: '', imagePath: '../../../assets/images/hero/wall4.jpg' }
     this.changeSlide()
   }
 
@@ -46,10 +55,10 @@ export class HeroComponent implements OnInit {
 
   changeSlide() {
     setInterval(() => {
-      if(this.slide != 'slide4')
-        this.slide = this.slides[this.slides.indexOf(this.slide) + 1]
+      if (this.states[this.slideCounter] != 'slide4')
+        this.slideCounter++
       else
-        this.slide = 'slide1'
-    }, 4000);
+        this.slideCounter = 0;
+    }, 6000);
   }
 }
