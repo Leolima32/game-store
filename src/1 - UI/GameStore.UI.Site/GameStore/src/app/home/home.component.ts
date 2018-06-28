c5119acaa2e56be514e0112f8e8c15df049017bc
lferreira_ext <lferreira_ext@BLGWLFERREIRA>
2018-06-28 17:30:12 -0300

    best seller games list in home

--- src/1 - UI/GameStore.UI.Site/GameStore/src/app/home/home.component.ts
+++ src/1 - UI/GameStore.UI.Site/GameStore/src/app/home/home.component.ts
@@ -4,7 +4,8 @@ import { Game } from '../games/game.model';
 
 @Component({
   selector: 'gs-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
