3fc9e3b5b9a803f02d596451f42840a9cd106931
Leonardo de Lima Ferreira <leonardo.ferreira-ext@viavarejo.com.br>
2020-02-21 16:34:37 -0300

    add genre service

--- src/1 - UI/GameStore.UI.Site/GameStore/src/app/genres/addgenre/addgenre.component.ts
+++ src/1 - UI/GameStore.UI.Site/GameStore/src/app/genres/addgenre/addgenre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AddGenreService } from './addgenre.service';
 
 @Component({
   selector: 'gs-addgenre',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddGenreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: AddGenreService) { }
 
   ngOnInit() {
   }
